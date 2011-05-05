1f470172604fc2b7dcb8044e525554b527f330c9
Mark Sonnabaum <mark@sonnabaum.com>
2011-05-04 19:33:05 -0500

    Added iptables rules for jenkins proxies.

--- cookbooks/jenkins/metadata.rb
+++ cookbooks/jenkins/metadata.rb
@@ -3,5 +3,6 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.5"
-%w(runit java iptables).each { |cb| depends cb }
+version          "0.5.1"
+#%w(runit java iptables).each { |cb| depends cb }
+%w(java iptables).each { |cb| depends cb }
